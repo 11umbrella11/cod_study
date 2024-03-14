ab8766c15a53b24d4419b14488ebe831444954f8
sspokopoko22 <sspokopoko22@gmail.com>
2024-03-14 15:58:34 +0900

    240314_7

--- class/pro_w/st02_3.cpp
+++ class/pro_w/st02_3.cpp
@@ -7,6 +7,26 @@ int main() {
   cout <<" **중국집**";
   cout << "짬뽕:1, 짜장:2, 탕수육: 3, 종료:4>> ";
   cin >> a;
+  /*while(true){
+    cout <<"짬뽕:1, 짜장:2, 탕수육: 3, 종료:4>> ";
+    cin >> menu;
+    if (menu <1||menu>4){
+        cout << "메뉴선택";
+        if (menu ==4){
+            cout <<" 영업종료, 감사합니다";
+            break;
+        }
+        그 외에는 continue;
+
+    아니면 스위치문
+    switch(menu){
+        case1:
+        cout~
+        break;
+    }
+
+    }
+  }*/
   if (a==1) {
     cout << "몇그릇?";
     cin >> b;
