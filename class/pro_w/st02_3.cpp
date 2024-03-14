42f91a309dfac575fffb3efa870ce2def68cf1df
sspokopoko22 <sspokopoko22@gmail.com>
2024-03-14 16:08:40 +0900

    f

--- class/pro_w/st02_3.cpp
+++ class/pro_w/st02_3.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 //#include <string>
 using namespace std;
+bool checkInputError(){//잘못된 타입이 입력되었을 경우를 예시
+    if(cin.fail()){
+        cin.clear();
+        cin.ignore(128,'\n');
+        cout<<"입력오류"<<endl;
+        return true;
+    }
+    else return false;
+    
+}
 int main() {
+
+    
+
+    /*
   int a=0 , b=0;
   
   cout <<" **중국집**";
@@ -10,6 +24,9 @@ int main() {
   /*while(true){
     cout <<"짬뽕:1, 짜장:2, 탕수육: 3, 종료:4>> ";
     cin >> menu;
+    if(checkInputError())//여기서 에러가 발생했을경우를 확인
+     //예를 들어 정수가 아닌 문자값을 입력했을경우 입력오류를 출력하여 확인캐함
+        continue;
     if (menu <1||menu>4){
         cout << "메뉴선택";
         if (menu ==4){
@@ -27,7 +44,7 @@ int main() {
 
     }
   }*/
-  if (a==1) {
+  /*if (a==1) {
     cout << "몇그릇?";
     cin >> b;
     cout <<"짬뽕"<<b<<"그릇 나왔습니다.";
@@ -47,5 +64,5 @@ int main() {
     
   else {
     cout <<" 잘못 입력하셨습니다.";
-    }
+    }*/
 }
