ba140834c99b97b48c625b27cbb5dd2a331a61d0
11umbrella11 <sspokopoko22@gmail.com>
2024-03-21 13:54:04 +0900

    240321_3

--- cod_study/lan/cp/q1.cpp
+++ cod_study/lan/cp/q1.cpp
@@ -19,11 +19,23 @@ int main(){
     //cout<<"3+8="<<sum<<endl;
 
     //4.변하지않는 값(상수)출력
-    const int GREAT_VICTORY_SALSU =612; //const: 상수_만약 여기에 새로 값을 할당하면"식이 수정할 수 있는 1value여야합니다"라고 출력.
-    const int GREAT_VICTORY_GWIJU =1019;
+    //const int GREAT_VICTORY_SALSU =612; //const: 상수_만약 여기에 새로 값을 할당하면"식이 수정할 수 있는 1value여야합니다"라고 출력.
+    //const int GREAT_VICTORY_GWIJU =1019; //현업에서는 상수이름을 모두 대문자로 표시
+    //cout<<"고구려 살수대첩의 년도는 "<<GREAT_VICTORY_SALSU<<"년도 입니다\n";
+    //cout<<"고려 귀주대첩의 년도는" <<GREAT_VICTORY_GWIJU<<"년도 입니다"<<endl;
 
-    cout<<"고구려 살수대첩의 년도는 "<<GREAT_VICTORY_SALSU<<"년도 입니다\n";
-    cout<<"고려 귀주대첩의 년도는" <<GREAT_VICTORY_GWIJU<<"년도 입니다"<<endl;
+    //5.사칙연산
+    int fst = 2;
+    int scd = 5;
+    int trd = 3;
+    int frt = 9;
 
+    cout << "2+5="<<fst+scd<<"\n"<<endl;
+    cout << "5-3="<<scd-trd<<"\n"<<endl;
+    cout << "3*9="<<trd*frt<<"\n"<<endl;
+    cout << "9/2="<<frt/fst<<"\n"<<endl;
+
+    cout <<"2+5*3="<<fst+scd*trd<<"\n"<<endl;
+    cout <<"9-5/3="<<frt-scd/trd<<"\n"<<endl;
     return 0;//프로그램 종료 
 }
