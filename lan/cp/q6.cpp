5c1edc631e604bad95ab9f1b840e60697b1e0636
11umbrella11 <sspokopoko22@gmail.com>
2024-04-02 15:43:42 +0900

    240402_5

--- lan/cp/q6.cpp
+++ lan/cp/q6.cpp
@@ -18,10 +18,11 @@ int main(){
 */
 
 //27. 쉼표 연산자 이해하기(,)
-
+/*
 int main(){
 
-    int goguryeo = 37, baekjae = 18, silla = 57;
+    int goguryeo = 37, baekjae = 18, silla = 57; 
+    //이건 printf의경우에만 가능!!
 
     printf("삼국 건국연도\n");
     printf("고구려bc%d년, 백제 bc%d년, 신라 bc%d년\n", goguryeo, baekjae, silla);
@@ -29,4 +30,42 @@ int main(){
     return 0;
 
 }
+*/
+//비트 연산자 이해하기
+#include<bitset>
+
+int main(){
+    bitset<8> bit1;//bitset의 변수를 선언하며 크기는 8로 할당합니다.
+    bit1.reset(); //0000 0000 
+    bit1=127; //0111 1111
+
+    bitset<8> bit2;
+    bit2.reset();//0000 0000
+    bit2=0x20; //32
+
+    bitset<8> bit3 = bit1 & bit2;//AND연산
+    bitset<8> bit4 = bit1 | bit2;//OR연산
+    bitset<8> bit5 = bit1 ^ bit2;//XOR연산
+    bitset<8> bit6 = ~bit1;//REVERSE
+    bitset<8> bit7 = bit2 << 1;//A>>n:A의 모든 비트를 n만큼 오른쪽 시프트
+    bitset<8> bit8 = bit2 >> 1;//A>>n:A의 모든 비트를 n만큼 왼쪽 시프트
+
+    cout<<"bit1&bit2 :"<<bit3<<","<<bit3.to_ulong()<<endl;
+    cout<<"bit1|bit2 :"<<bit4<<","<<bit4.to_ulong()<<endl;
+    cout<<"bit1^bit2 :"<<bit5<<","<<bit5.to_ulong()<<endl;
+    cout<<"~bit1 :"<<bit6<<","<<bit6.to_ulong()<<endl;
+    cout<<"bit2<<1 :"<<bit7<<","<<bit7.to_ulong()<<endl;
+    cout<<"bit2>>1 :"<<bit8<<","<<bit8.to_ulong()<<endl;
+
+    return 0;
+
+}
+/*결과값:
+bit1&bit2 :00100000,32
+bit1|bit2 :01111111,127
+bit1^bit2 :01011111,95
+~bit1 :10000000,128
+bit2<<1 :01000000,64
+bit2>>1 :00010000,16
+*/
 
