f1005631183eceb5a48b1b6c09bafaa8842db468
11umbrella11 <sspokopoko22@gmail.com>
2024-04-02 16:21:13 +0900

    240402_6

--- lan/cp/q6.cpp
+++ lan/cp/q6.cpp
@@ -32,6 +32,7 @@ int main(){
 }
 */
 //비트 연산자 이해하기
+/*
 #include<bitset>
 
 int main(){
@@ -41,7 +42,7 @@ int main(){
 
     bitset<8> bit2;
     bit2.reset();//0000 0000
-    bit2=0x20; //32
+    bit2=0x20; //32=0010 0000
 
     bitset<8> bit3 = bit1 & bit2;//AND연산
     bitset<8> bit4 = bit1 | bit2;//OR연산
@@ -60,6 +61,7 @@ int main(){
     return 0;
 
 }
+*/
 /*결과값:
 bit1&bit2 :00100000,32
 bit1|bit2 :01111111,127
@@ -69,3 +71,30 @@ bit2<<1 :01000000,64
 bit2>>1 :00010000,16
 */
 
+//29. 캐스트 연산자 이해하기(static_cast)
+
+int main(){
+
+    int x = 2;
+    double y = 4.4;
+
+    int i = static_cast<int>(y/x);//형변환 에러를 막기위한 변환 연산자
+    int j = (int)y/x;//타입지정
+    double k = y / x;
+
+    cout << "4.4/2 = (static_cast<int>)"<<i<<endl;
+    cout << "4.4/2 = (int)"<<j<<endl;
+    cout << "4.4/2 = "<<k<<endl;
+
+    return 0;
+}
+//static_cast<>: 기본적인 cast
+//dynamic_cast<>: 객체지향언어의 다형성을 이용하여 모호한 타입의 캐스트오류를 막아줌
+//const_cast: 자료혀이 갖고있는 상수속성을 제거
+//reinterpret_cast<>: 포인터타입끼리도 변환할 수 있게 도와줌
+/*
+결과값
+4.4/2 = (static_cast<int>)2
+4.4/2 = (int)2
+4.4/2 = 2.2
+*/
