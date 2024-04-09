700dc44356e4620130e9761e4687ba24a8383de8
11umbrella11 <sspokopoko22@gmail.com>
2024-04-09 15:22:08 +0900

    240409_4

---
+++ class/pro_w/st06_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+//함수원형 선언
+void star(int a);
+void msg(int id, string text);
+
+int main(){
+    star();//star(5)
+    star(15);
+    msg(1004);
+    msg(2004, "computer");
+}
+
+void star(int a=5){
+    for(int i=5){
+        cou<<"*";
+        cout<<endl;
+
+}
+void msg(int  id, string text =""){
+    cout<<id<<":"<<test<<endl;
+}
+
+}
