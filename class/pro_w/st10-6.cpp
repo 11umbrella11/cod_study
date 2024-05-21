2965a8fb349a733ace5830b3151c3696a5f8a1d4
cactus33 <160423884+11umbrella11@users.noreply.github.com>
2024-05-21 05:25:24 +0000

    st10_6

---
+++ class/pro_w/st10-6.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printvector(vector<int> &v){
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<' ';
+    cout<<endl;
+
+}
+
+double getAvg(vector<int> &v){
+    int sum=0;
+    for(auto it=v.begin());
+    //vector<int>::iterator이라 쓸것을 바꿈
+        sum+=*it;
+    return (double) sum/v.size();
+
+}
+int main(){
+    vector<int> v;
+    
+    while(true){
+        cout<<"정수를 입력하시오(0입력시 종료):";
+        int num;
+        cin>>num;
+        if(num==0) break;
+        v.push_back(num);
+        printVector(v);
+        cout<<"평균:"<<getAvg<<endl;
+        
+
+    }
+
+
+
+
+
+
+
+
+/*int sum=0, num=0;
+    double avr=0;
+    cout<<"정수를 입력하세요(0을 입력하면 종료):";
+    cin>>v.push_back(1);
+    num++;
+    while(true){
+        if(v!=0)(
+            v+=sum
+            avr=sum/num;
+
+        )
+        else
+        break;
+    
+    }*/
+    
+}
