a417bccc896fe1409e3bc70c6b56470c03bd1d49
cactus33 <160423884+11umbrella11@users.noreply.github.com>
2024-05-21 05:45:00 +0000

    fir_st10-7

---
+++ class/pro_w/st10_7.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Book{
+    public:
+    Book();
+
+}
+int main(){
+    vector<Book> v;
+    int yr=0, num=0;
+    string wt,nm;
+    while(true){
+        if(yr=-1) break;
+        cout<<"입고할 책을 입력하세요. 년도에 -1을 입력하면 종료됩니다.";
+        cout<<"년도>>";
+        cin<<yr;
+         if(yr=-1) break;
+        cout<<"책이름>>";
+        cin
+    }
+    
+}
