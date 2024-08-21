7c3607c65e2ef62181013002a6603f7c4c412513
11umbrella11 <sspokopoko22@gmail.com>
2024-08-21 22:21:31 +0900

    0821_1

--- lan/cp/A_02.basic/q14.cpp
+++ lan/cp/A_02.basic/q14.cpp
@@ -58,12 +58,12 @@ number2: 34.2
 using namespace std;
 
 int main(){
-    auto result1 = min(1,5);
-    auto result2 = max('a', 'z');
-    cout<<result1<<","<<result2<<endl;
+    //auto result1 = min(1,5);
+    //auto result2 = max('a', 'z');
+    //cout<<result1<<","<<result2<<endl;
 
-    auto result3 = minmax({'a','n','z'});
-    auto result4 = minmax({1,2,3});
+    auto result3 = minmax({'l','e','k'});
+    auto result4 = minmax({4, 9, 2});
 
     cout<<result3.first<<","<<result3.second<<endl;
     cout<<result4.first<<","<<result4.second<<endl;
