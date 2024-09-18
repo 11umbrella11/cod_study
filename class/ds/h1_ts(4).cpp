97df18479c1c24309db9e447c3c544c0287fe973
11umbrella11 <sspokopoko22@gmail.com>
2024-09-18 19:29:09 +0900

    w3

--- class/ds/h1_ts(4).cpp
+++ class/ds/h1_ts(4).cpp
@@ -5,6 +5,49 @@
 
 using namespace std;
 
+void DeleteDup(int arr[], int num){
+
+    //int o=sizeof(arr)/sizeof(arr[0]);
+    unordered_set<int> notDup(arr, arr+num);
+    
+    int dupCheck=0;
+    //int notDupArr[notDup.size()];
+    
+    /*for(int n=0;n<notDup.size();n++){
+        cout<<notDupArr[dupCheck]<<"hh";
+        notDupArr[i]=not;*/
+
+        //cout<<notDupArr[s_size]<<endl;
+    //}
+    cout<<"->"<<notDup.size()<<"개"<<"(";
+    //sort(notDup, notDup.size());
+    for(int i:notDup){
+        cout<<" "<<i;
+    }
+    cout<<")";
+    
+}
+
+
+int* CheckPN(int arr[], int num){
+    int check[10]={2,3,5,7,11,13,17,19,23,29};
+    for(int i=0;i<num;i++){
+        if(arr[i]==0||arr[i]==1)
+            arr[i]=0;
+        for(int j=0;j<10;j++){
+            if(arr[i]!=check[j])
+                if(arr[i]%check[j]==0)
+                    arr[i]=0;
+        }          
+    }
+    sort(arr,arr+num);
+    for(int i=0;i<num;i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return arr;
+}
+    
 void Pmt(int arr[], int num){
     int tm[300]={};
     int pmt[1000]={};
@@ -31,59 +74,9 @@ void Pmt(int arr[], int num){
         }
     }
     //모든 경우의 순열 생성
-    //sort(pmt, pmt+m);
-    //for(int i=0;i<m;i++){
-    //    cout<<pmt[i]<<"k"<<endl;
-    //}
-    int o=sizeof(pmt)/sizeof(pmt[0]);
-    unordered_set<int> notDup(pmt, pmt+m);
-    //for(int i=0;i<o;i++){
-    //    notDup.insert(pmt[i]);
-    //    cout<<pmt[i]<<endl;
-    //}
-
-    //int s_size=0;
-    int notDupArr[notDup.size()];
-    //for(auto n=notDup.begin();n!=notDup.end();n++){
-    //    notDupArr[s_size++]=*n;
-        //cout<<notDupArr[s_size]<<endl;
-    //}
-    //for(int i:notDup){
-    //    cout<<i<<endl;
-    //}
+    int* pn=CheckPN(pmt, m);
+    DeleteDup(pmt, m);
     
-}
-
-
-
-void CheckPN(int arr[]){
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]==0||arr[i]==1)
-            
-    }
-}
-
-void DeleteDup(int arr[]){
-    //sort(pmt, pmt+m);
-    //for(int i=0;i<m;i++){
-    //    cout<<pmt[i]<<"k"<<endl;
-    //}
-    int o=sizeof(pmt)/sizeof(pmt[0]);
-    unordered_set<int> notDup(pmt, pmt+m);
-    //for(int i=0;i<o;i++){
-    //    notDup.insert(pmt[i]);
-    //    cout<<pmt[i]<<endl;
-    //}
-
-    //int s_size=0;
-    int notDupArr[notDup.size()];
-    //for(auto n=notDup.begin();n!=notDup.end();n++){
-    //    notDupArr[s_size++]=*n;
-        //cout<<notDupArr[s_size]<<endl;
-    //}
-    //for(int i:notDup){
-    //    cout<<i<<endl;
-    //}
 }
 
 
@@ -99,7 +92,7 @@ int main() {
         arr[m]=inp[m]-'0';
         //정수형으로 변경함
     }
-    sort(arr,arr+k);
+    //sort(arr,arr+k);
     Pmt(arr, k);
     return 0;
 }
